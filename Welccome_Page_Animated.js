bc80582a50b0751c961d8d4dd3d2e1da1c65674d
ankitha75 <ankithakrishnareddyb@gmail.com>
2025-03-20 10:06:01 +0530

    Add files via upload

---
+++ Welccome_Page_Animated.js
@@ -0,0 +1,4 @@
+// If you'd like to make the button interactive or add other functionality
+document.querySelector('.learn-more-btn').addEventListener('click', function() {
+    alert("Stay tuned for more information!");
+});
